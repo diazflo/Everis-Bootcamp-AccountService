bd4b11bb87626cfeba7f76d7636ed880d12835cc
diazflo <bdiazflo@everis.com>
2021-06-24 17:33:24 -0500

    adicion de Dockerfile, y registro en EurekaServer, problema con el server config no permitia poder lanzar utilizar las apis mediante esta.

--- account/src/main/java/com/everis/account/AccountApplication.java
+++ account/src/main/java/com/everis/account/AccountApplication.java
@@ -1,14 +1,12 @@
 package com.everis.account;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.DisposableServer;
-import reactor.netty.tcp.TcpServer;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class AccountApplication {
 
