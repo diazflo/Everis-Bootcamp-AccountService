9eee069201c754f71d12813cf242e0a6bade981f
diazflo <bdiazflo@everis.com>
2021-06-26 22:04:40 -0500

    actualizacion de url

--- account/src/main/java/com/everis/account/controller/HomeController.java
+++ account/src/main/java/com/everis/account/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/home/")
+@RequestMapping("/bank/account/home/")
 public class HomeController {
 
     @GetMapping
